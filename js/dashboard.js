915b66631c399d8cbed8f780c9180c0bfae139fc
davojta <damarah@gmail.com>
2016-10-29 15:53:50 +0300

    add json  base rendering

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,5 +2,18 @@ $(function () {
     console.log('start!')
     $.get('/city-dashboard/data/test.json', function (data) {
         console.log(data)
+
+        var template = `<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h4>Забруджванне</h4>
+						<div class="easypiechart" id="easypiechart-blue" data-percent="${data.pollution}" ><span class="percent">${data.pollution}%</span>
+						</div>
+					</div>
+				</div>`
+
+        $('#chart_pollution').html(template)
+
+
+
     })
 })
