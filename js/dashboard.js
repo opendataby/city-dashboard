230bf60e5b23b304cd470f2d13e40aa0e978e717
Konstantin Reido <konstantin.reido@gmail.com>
2016-11-05 21:44:27 +0300

    Add division of indicators by districts

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,15 @@
 $(function () {
     $.get('/city-dashboard/data/frontend.json', function (data) {
       var template = '';
-      $.each(data[0]['params'], function(index, params) {
-        template += indicator(params);
+      $.each(data[0]['districts'], function(index, district) {
+        console.log(district['name'])
+        template += "<h1>" + district['name'] + "</h1>";
+        $.each(district['indicators'], function(index, indicator) {
+          template += buildTemplateIndicator(indicator);
+        });
       });
 
-      $('#indicators').html(template);
+      $('#dashboard').html(template);
 
       $('.easypiechart-teal').easyPieChart({
         scaleColor: false,
@@ -19,16 +23,16 @@ $(function () {
     });
 });
 
-function indicator(params){
+function buildTemplateIndicator(indicator){
   // One more color: orange
-  var color = params['value'] <= params['average'] ? 'teal' : 'red';
-  var unit = params['unit'] !== undefined ? params['unit'] : ''
+  var color = indicator['value'] <= indicator['average'] ? 'teal' : 'red';
+  var unit = indicator['unit'] !== undefined ? indicator['unit'] : ''
 
   return `<div class="col-xs-6 col-md-3">
             <div class="panel panel-default">
               <div class="panel-body easypiechart-panel">
-                <h4>${params['parameter']}</h4>
-                <div class="easypiechart easypiechart-${color}" data-percent="${params['value']}" ><span class="percent">${params['value']} ${unit}</span>
+                <h4>${indicator['name']}</h4>
+                <div class="easypiechart easypiechart-${color}" data-percent="${indicator['value']}" ><span class="percent">${indicator['value']} ${unit}</span>
                 </div>
               </div>
             </div>
